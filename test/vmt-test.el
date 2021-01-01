3cda347b97d8255c96624e901e45dca649289221
ROCKTAKEY <rocktakey@gmail.com>
2021-01-01 16:24:46 +0900

    Initial commit

---
+++ test/vmt-test.el
@@ -0,0 +1,38 @@
+;;; vmt-test.el --- Test for vmt                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords:
+
+;; Version: 0.0.0
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'undercover)
+(undercover "*.el"
+            (:report-file "coverage-final.json")
+            (:send-report nil))
+
+(require 'vmt)
+
+
+(provide 'vmt-test)
+;;; vmt-test.el ends here
